f9eecd3caa4f9b7713c6ab02a08659d6a8a18c25
Gilbert <gssenyonjo@gmail.com>
2022-04-24 15:44:56 +0300

    Initial commit

---
+++ src/main/java/net/passioncloud/taming/TamingApplication.java
@@ -0,0 +1,13 @@
+package net.passioncloud.taming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamingApplication.class, args);
+    }
+
+}
